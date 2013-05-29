d1503167515336c295e1bc3aeb05aa8e25ff0305
jroglesby <jro08a@gmail.com>
2013-05-28 22:36:16 -0400

    Created initial webapi for NewDayNewLeaf.com

---
+++ NewDayNewLeaf/Controllers/FishController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewDayNewLeaf.Models;
+
+namespace NewDayNewLeaf.Controllers
+{
+    public class FishController : Controller
+    {
+        //
+        // GET: /Fish/
+
+        public Fish Get(string fishName)
+        {
+            Fish seaBass = new Fish();
+            seaBass.FishName = "Sea Bass";
+            seaBass.Price = 300;
+            seaBass.Rarity = "Very fucking common";
+            seaBass.ShadowSize = "Huge, just to troll you.";
+            seaBass.CheesySaying = "I caught a sea bass! Oh man, not again!";
+
+            return seaBass;
+        }
+        public Fish Get()
+        {
+            Fish seaBass = new Fish();
+            seaBass.FishName = "Sea Bass";
+            seaBass.Price = 300;
+            seaBass.Rarity = "Very fucking common";
+            seaBass.ShadowSize = "Huge, just to troll you.";
+            seaBass.CheesySaying = "I caught a sea bass! Oh man, not again!";
+
+            return seaBass;
+        }
+
+    }
+}
---
+++ NewDayNewLeaf/Models/Fish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewDayNewLeaf.Models
+{
+    public class Fish
+    {
+        public string FishName;
+        public int Price;
+        public string ShadowSize;
+        public string Rarity;
+        public string CheesySaying;
+    }
+}
