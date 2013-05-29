ca87ae8ab7b20c1cfcaa16e7e028638861cf8da0
jroglesby <jro08a@gmail.com>
2013-05-28 22:40:38 -0400

    Update routes and remove unnecessary files

--- NewDayNewLeaf/App_Start/WebApiConfig.cs
+++ NewDayNewLeaf/App_Start/WebApiConfig.cs
@@ -14,6 +14,10 @@ namespace NewDayNewLeaf
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Routes.MapHttpRoute(
+                name: "Fish",
+                routeTemplate: "api/Fish/fishName",
+            );
 
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
