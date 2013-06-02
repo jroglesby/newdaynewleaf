c9c85ae0c9c8526f07f426159077cc6ef649c936
jroglesby <jro08a@gmail.com>
2013-06-02 11:41:38 -0400

    Preliminary FishInfo controller and view, add "inspect" link to home page listing.

--- NewDayNewLeaf/App_Start/RouteConfig.cs
+++ NewDayNewLeaf/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace NewDayNewLeaf
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "FishInfo",
+                url: "FishInfo/{fishName}",
+                defaults: new { controller = "FishInfo", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
